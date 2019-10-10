55f747ee346b094a01cad716f882047e6a5a3bb0
yangguanghui <yangguanghui@bensen.com>
2019-10-10 19:04:58 +0800

    first commit

---
+++ IPExample/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace IPExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLocal_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string name = Dns.GetHostName();
+            listBox1.Items.Add("本机主机名：" + name);
+            IPHostEntry me = Dns.GetHostEntry(name);
+            listBox1.Items.Add("本机所有IP 地址：");
+            foreach (IPAddress ip in me.AddressList)
+            {
+                listBox1.Items.Add(ip);
+            }
+            IPAddress localip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint iep = new IPEndPoint(localip, 80);
+            listBox1.Items.Add("The IPEndPoint is: " + iep.ToString());
+            listBox1.Items.Add("The Address is: " + iep.Address);
+            listBox1.Items.Add("The AddressFamily is: " + iep.AddressFamily);
+            listBox1.Items.Add("The max port number is: " + IPEndPoint.MaxPort);
+            listBox1.Items.Add("The min port number is: " + IPEndPoint.MinPort);
+        }
+
+        private void buttonRemote_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            IPHostEntry remoteHost = Dns.GetHostEntry("www.cctv.com");
+            IPAddress[] remoteIP = remoteHost.AddressList;
+            listBox1.Items.Add("中央电视台：");
+            foreach (IPAddress ip in remoteIP)
+            {
+                IPEndPoint iep = new IPEndPoint(ip, 80);
+                listBox1.Items.Add(iep);
+            }
+        }
+    }
+}
+
